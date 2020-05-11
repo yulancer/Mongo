22b02869df9485b22f9fc5615325e292eeb6d51f
Yuriy <yulancer@yandex.ru>
2020-05-11 12:31:36 +0300

    Add project files.

---
+++ Mongo/Program.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Mongo
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Console.ReadLine();
+        }
+    }
+}
